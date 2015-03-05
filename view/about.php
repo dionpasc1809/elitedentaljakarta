ee4b5a2717ad0cbc430a86617f83adde4cc63438
Dion Pascal N <nursukmad@garena.com>
2015-03-05 09:04:36 +0700

    Pushed 20150305 - 0904 -- Added [Doctors]

--- view/about.php
+++ view/about.php
@@ -1,7 +1,84 @@
 <div id="thedoctors">
     <section id="doctor-container">
         <div class="container">
+            <div style="width: 100%; height: 50px;"></div>
+            <div class="doctor-head">
+                <div class="doctor-logo">
+                    <img src="images/home/logo.png"/>
+                </div>
+                <div class="doctor-head-title">
+                    The Doctors
+                </div>
+            </div>
+            <div class="doctor-content">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="doctor-outside-item">
+                            <div class="doctor-item">
+                                <img class="doctor-item-photo" src="images/doctors/photos/drg-niken.jpg"/>
+                                <div class="doctor-btn">
+                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
+                    <div class="col-sm-4">
+                        <div class="doctor-outside-item">
+                            <div class="doctor-item">
+                                <img class="doctor-item-photo" src="images/doctors/photos/drg-putut.jpg"/>
+                                <div class="doctor-btn">
+                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="doctor-outside-item">
+                            <div class="doctor-item">
+                                <img class="doctor-item-photo" src="images/doctors/photos/drg-kayla.jpg"/>
+                                <div class="doctor-btn">
+                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="doctor-outside-item">
+                            <div class="doctor-item">
+                                <img class="doctor-item-photo" src="images/doctors/photos/drg-dyta.jpg"/>
+                                <div class="doctor-btn">
+                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="doctor-outside-item">
+                            <div class="doctor-item">
+                                <img class="doctor-item-photo" src="images/doctors/photos/drg-david.jpg"/>
+                                <div class="doctor-btn">
+                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="doctor-outside-item">
+                            <div class="doctor-item">
+                                <img class="doctor-item-photo" src="images/doctors/photos/drg-teuku.jpg"/>
+                                <div class="doctor-btn">
+                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </div>
