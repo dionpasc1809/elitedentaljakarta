c6606dcf0170f356054f44d78303864401ef6457
Dion Pascal N <nursukmad@garena.com>
2015-03-06 09:55:22 +0700

    Pushed 20150306 - 0955 -- Added doctor features

--- view/about.php
+++ view/about.php
@@ -17,7 +17,7 @@
                             <div class="doctor-item">
                                 <img class="doctor-item-photo" src="images/doctors/photos/drg-niken.jpg"/>
                                 <div class="doctor-btn">
-                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                    <a href="#" data-doctor="niken"><img src="images/doctors/btn-lup.png"/></a>
                                 </div>
                             </div>
                         </div>
@@ -28,7 +28,7 @@
                             <div class="doctor-item">
                                 <img class="doctor-item-photo" src="images/doctors/photos/drg-putut.jpg"/>
                                 <div class="doctor-btn">
-                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                    <a href="#" data-doctor="putut"><img src="images/doctors/btn-lup.png"/></a>
                                 </div>
                             </div>
                         </div>
@@ -39,7 +39,7 @@
                             <div class="doctor-item">
                                 <img class="doctor-item-photo" src="images/doctors/photos/drg-kayla.jpg"/>
                                 <div class="doctor-btn">
-                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                    <a href="#" data-doctor="kayla"><img src="images/doctors/btn-lup.png"/></a>
                                 </div>
                             </div>
                         </div>
@@ -50,7 +50,7 @@
                             <div class="doctor-item">
                                 <img class="doctor-item-photo" src="images/doctors/photos/drg-dyta.jpg"/>
                                 <div class="doctor-btn">
-                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                    <a href="#" data-doctor="dyta"><img src="images/doctors/btn-lup.png"/></a>
                                 </div>
                             </div>
                         </div>
@@ -61,7 +61,7 @@
                             <div class="doctor-item">
                                 <img class="doctor-item-photo" src="images/doctors/photos/drg-david.jpg"/>
                                 <div class="doctor-btn">
-                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                    <a href="#" data-doctor="david"><img src="images/doctors/btn-lup.png"/></a>
                                 </div>
                             </div>
                         </div>
@@ -72,13 +72,43 @@
                             <div class="doctor-item">
                                 <img class="doctor-item-photo" src="images/doctors/photos/drg-teuku.jpg"/>
                                 <div class="doctor-btn">
-                                    <a href="#"><img src="images/doctors/btn-lup.png"/></a>
+                                    <a href="#" data-doctor="teuku"><img src="images/doctors/btn-lup.png"/></a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="popupDoctors" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+
+                        <!--<div class="modal-header">
+                            <h4 class="modal-title" id="myModalLabel">
+                                This Modal title
+                            </h4>
+                        </div>-->
+                        <div class="modal-body">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times; </button>
+                            Add some text here
+                        </div>
+                        <!--<div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary"> Submit changes</button>
+                        </div>-->
+                    </div><!-- /.modal-content -->
+                </div><!-- /.modal -->
+            </div>
         </div>
     </section>
+    <script type="text/javascript">
+        $('.doctor-btn a').click(function(e) {
+            var doctor_attr = $(this).attr('data-doctor');
+//            $('#popupDoctors .modal-header .modal-title').html(doctor_attr);
+            $('#popupDoctors').modal('show');
+            e.preventDefault();
+        });
+    </script>
 </div>
