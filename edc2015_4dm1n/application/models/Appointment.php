c28443a80dd70a6d4241bcf69ac18be5a0c8ff27
Dion.Pascal.N <godheaper@gmail.com>
2015-03-29 23:52:58 +0700

    Push 20150329 - 2348 -- Remove old files

--- edc2015_4dm1n/application/models/Appointment.php
+++ edc2015_4dm1n/application/models/Appointment.php
@@ -14,7 +14,17 @@ class Appointment extends CI_Model {
 
     function get_by_id($id) {
         $this->db->where('id',$id);
-        return $this->db->get($this->_table_name)->result();
+        return $this->db->get($this->_table_name)->row();
+    }
+
+    function save_edit($data, $id, $idval) {
+        $this->db->where($id,$idval);
+        $this->db->update($this->_table_name,$data);
+    }
+
+    function delete_data($id) {
+        $this->db->where('id',$id);
+        $this->db->delete($this->_table_name);
     }
 
 }
--- edc2015_4dm1n/application/models/Doctors.php
+++ edc2015_4dm1n/application/models/Doctors.php
@@ -18,4 +18,19 @@ class Doctors extends CI_Model {
         return $this->db->get($this->_table_name)->result();
     }
 
+    function get_by_id($id) {
+        $this->db->where('id',$id);
+        return $this->db->get($this->_table_name)->row();
+    }
+
+    function save_edit($data, $id, $idval) {
+        $this->db->where($id,$idval);
+        $this->db->update($this->_table_name,$data);
+    }
+
+    function delete_data($id) {
+        $this->db->where('id',$id);
+        $this->db->delete($this->_table_name);
+    }
+
 }
